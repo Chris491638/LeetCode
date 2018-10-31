6b1a6cec2ed3cca02359a59fdbc941f81ed2d525
chris <chris940406@gmail.com>
2018-10-31 14:08:31 +0800

    0146_LRUCache_Go2

--- 0146/LRUCache.go
+++ 0146/LRUCache.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 )
 
+/*
 // 328ms 7.62% chris
 type Node struct {
 	key   int
@@ -69,26 +70,111 @@ func (this *LRUCache) PrintAll() {
 	}
 	fmt.Println()
 }
+*/
+
+// 316ms 7.62% chris
+type Node struct {
+	prev  *Node
+	key   int
+	value int
+	next  *Node
+}
+
+type LRUCache struct {
+	head *Node
+}
+
+func Constructor(capacity int) LRUCache {
+	node := &Node{nil, capacity, 0, nil}
+	node.prev = node
+	node.next = node
+	return LRUCache{node}
+}
+
+func (this *LRUCache) Get(key int) int {
+	current := this.head
+	for current.next != this.head {
+		p := current.next
+		if p.key == key {
+			current.next = p.next
+			p.next.prev = current
+			p.next = this.head.next
+			p.prev = this.head
+			this.head.next.prev = p
+			this.head.next = p
+			return p.value
+		} else {
+			current = current.next
+		}
+	}
+	return -1
+}
+
+func (this *LRUCache) Put(key int, value int) {
+	current := this.head
+	for current.next != this.head {
+		p := current.next
+		if p.key == key {
+			p.value = value
+			current.next = p.next
+			p.next.prev = current
+			p.next = this.head.next
+			p.prev = this.head
+			this.head.next.prev = p
+			this.head.next = p
+			return
+		} else {
+			current = current.next
+		}
+	}
+	if this.head.key == this.head.value {
+		p := this.head.prev
+		p.key = key
+		p.value = value
+
+		p.prev.next = this.head
+		this.head.prev = p.prev
+
+		p.next = this.head.next
+		this.head.next.prev = p
+
+		p.prev = this.head
+		this.head.next = p
+	} else {
+		node := &Node{this.head, key, value, nil}
+
+		this.head.next.prev = node
+		node.next = this.head.next
+		this.head.next = node
+		node.prev = this.head
+
+		this.head.value += 1
+	}
+}
+
+func (this *LRUCache) PrintAll() {
+	fmt.Printf("cap=%d;len=%d\n", this.head.key, this.head.value)
+	for current := this.head; current.next != this.head; current = current.next {
+		fmt.Printf("key=%d;value=%d\n", current.next.key, current.next.value)
+	}
+	fmt.Println()
+}
 
 func main() {
 	obj := Constructor(2)
+	obj.Put(2, 1)
+	obj.PrintAll()
 	obj.Put(1, 1)
 	obj.PrintAll()
-	obj.Put(2, 2)
+	obj.Put(2, 3)
 	obj.PrintAll()
+	obj.Put(4, 1)
+	obj.PrintAll()
+
 	obj.Get(1)
-	obj.PrintAll()
-	obj.Put(3, 3)
 	obj.PrintAll()
 	obj.Get(2)
 	obj.PrintAll()
-	obj.Put(4, 4)
-	obj.PrintAll()
-	obj.Get(1)
-	obj.PrintAll()
-	obj.Get(3)
-	obj.PrintAll()
-	obj.Get(4)
 }
 
 /**
